39f81fc31d753724fdbaad5d155aed65ac678235
eben-siaw <edei-siaw@st.ug.edu.gh>
2021-11-04 10:55:21 +0100

    new commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import './sign_up.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SignUpPage()
+    );
+  }
+}
+
